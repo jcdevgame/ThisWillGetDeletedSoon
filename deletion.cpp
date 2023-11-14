9326021233447206a00cefcfc1265930ab5f8575
JCGameDev <95277271+nvfjfbeveivg@users.noreply.github.com>
2023-11-14 10:45:34 -0500

    Update deletion.cpp

--- deletion.cpp
+++ deletion.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+using namespace std;
+
+void destruction();
+void displayScreen();
+
+char square[10] = {'O', '1', '3', '4', '5', '6', '7', '8', '9'};
 
 int main(){
   cout << "Welcome To Destruction Simulator";
-
   return 0;
 }
 
 
 void destruction(){
-  int destructionValue = 0
+  int destructionValue = 0;
   cout << "Enter the destruction power!";
   
   cin >> destructionValue;
-  
+}
+
+void displayScreen(){
+  system("cls");
 }
