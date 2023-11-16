c342e9409b37ad93a973818b547e7e53a813d3a8
JCGameDev <95277271+nvfjfbeveivg@users.noreply.github.com>
2023-11-16 09:51:45 -0500

    Update deletion.cpp

--- deletion.cpp
+++ deletion.cpp
@@ -4,10 +4,11 @@ using namespace std;
 void destruction();
 void displayScreen();
 
-char square[10] = {'O', '1', '3', '4', '5', '6', '7', '8', '9'};
+char square[10] = {'O', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 int main(){
-  cout << "Welcome To Destruction Simulator";
+  displayScreen();
+
   return 0;
 }
 
@@ -15,10 +16,23 @@ int main(){
 void destruction(){
   int destructionValue = 0;
   cout << "Enter the destruction power!";
-  
+
   cin >> destructionValue;
 }
 
 void displayScreen(){
-  system("cls");
+  system("CLS");
+
+  cout << "--------------------------------\n\n";
+  cout << "Welcome to Destruction Simulator \n\n";
+  cout << "Your goal is to destroy everything \n\n";
+  cout << " |   |   |   |\n";
+  cout << " | " << square[1] << " | " << square[2] << " | " << square[3] << " |\n";
+  cout << " |___|___|___|\n";
+  cout << " |   |   |   |\n";
+  cout << " | " << square[4] << " | " << square[5] << " | " << square[6] << " |\n";
+  cout << " |___|___|___|\n";
+  cout << " |   |   |   |\n";
+  cout << " | " << square[7] << " | " << square[8] << " | " << square[9] << " |\n";
+  cout << " |   |   |   |\n";
 }
